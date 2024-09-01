27ccff8ba42ecdcc3c31876776b25abccc8b8ccd
HarshalGourkar <harshalgourkar1804@gmail.com>
2024-09-01 20:34:08 +0530

    A Work done

--- a.java
+++ a.java
@@ -1,7 +1,12 @@
 public class A{
 
 	public void harshalWork(){
-		System.out.println("my work");
+		System.out.println("Harshal work");
 	}
+	
+	public void aWork(){
+		System.out.println("A work");
+	}
+	
 
 }
